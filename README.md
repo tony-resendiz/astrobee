4e7b14124478d6037546c5a05d6a4a31ffe60f98
tresendiz <tresendiz@cj.com>
2021-02-06 11:42:42 -0800

    Fixed render bug by saving state of flipped models.

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 
 TODOs
 - figure out if it can render a model twice from the same file
-- bug where it renders the models in different orientation on events?
-  - should there be some kind of cleanup?
 - flesh out testing
+- setup linter
--- src/Astrobee.js
+++ src/Astrobee.js
@@ -3,23 +3,43 @@ import {useLoader, useFrame} from 'react-three-fiber'
 import {ColladaLoader} from 'three/examples/jsm/loaders/ColladaLoader'
 import {Matrix4} from 'three'
 
+export const MODEL_FILES = [
+    "/body.dae",
+    "/pmc.dae",
+    "/pmc_2.dae",
+    "/pmc_bumper.dae",
+    "/pmc_bumper_2.dae",
+    "/pmc_skin_.dae",
+    "/pmc_skin_2.dae"
+]
+
+export const MODELS_TO_FLIP = [
+    "/pmc_2.dae",
+    "/pmc_bumper_2.dae",
+    "/pmc_skin_2.dae"
+]
 
 export const Astrobee = ({yRotation}) => {
+
     return (
         <Suspense
             fallback={<mesh/>}>
-            <DaeFromFile file={'/body.dae'} yRotation={yRotation}/>
-            <DaeFromFile file={'/pmc.dae'} yRotation={yRotation}/>
-            <DaeFromFile file={'/pmc_2.dae'} flipZ yRotation={yRotation}/>
-            <DaeFromFile file={'/pmc_bumper.dae'} yRotation={yRotation}/>
-            <DaeFromFile file={'/pmc_bumper_2.dae'} flipZ yRotation={yRotation}/>
-            <DaeFromFile file={'/pmc_skin_.dae'} yRotation={yRotation}/>
-            <DaeFromFile file={'/pmc_skin_2.dae'} flipZ yRotation={yRotation}/>
+            {
+                MODEL_FILES.map(file => {
+                    return <DaeFromFile
+                        key={file}
+                        file={file}
+                        yRotation={yRotation}
+                    />
+                })
+            }
         </Suspense>
     )
 }
 
-export const DaeFromFile = ({file, flipZ, yRotation}) => {
+export const DaeFromFile = ({file, yRotation}) => {
+
+    const [hasFlipped, setHasFlipped] = useState([])
 
     const mesh = useRef()
 
@@ -29,7 +49,10 @@ export const DaeFromFile = ({file, flipZ, yRotation}) => {
 
     const {scene} = useLoader(ColladaLoader, file)
 
-    if (flipZ) scene.applyMatrix4(new Matrix4().makeScale(1, 1, -1));
+    if (MODELS_TO_FLIP.includes(file) && !hasFlipped.includes(file)) {
+        scene.applyMatrix4(new Matrix4().makeScale(1, 1, -1))
+        setHasFlipped(hasFlipped.concat(file))
+    }
 
     return (
         <primitive
